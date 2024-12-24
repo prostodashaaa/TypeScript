78a6a9b8ef0d458aeb04202338d8588c68f6344e
Darya <kozyreva2007sv@gmail.com>
2024-12-24 15:05:24 +0300

    fix

--- 9-difference/difference.ts
+++ 9-difference/difference.ts
@@ -3,30 +3,28 @@ interface IA {
   b: string;
   d: boolean;
 }
-
 interface IB {
   a: number;
   c: boolean;
 }
 
-let a: IA = { a: 5, b: "", d: true };
+let a = { a: 5, b: "", d: true };
 let b: IB = { a: 10, c: true };
 
-function difference<T extends object, K extends object>(
+function Difference<T extends object, K extends object>(
   ReferenceObject: T,
   CompareObject: K
-): Pick<T, Exclude<keyof T, keyof K>> {
-  type UniqueKeys = Exclude<keyof T, keyof K>;
-
-  const result: Partial<T> = {};
+): { [Key in Exclude<keyof T, keyof K>]: T[Key] } {
+  const result = <{ [Key in Exclude<keyof T, keyof K>]: T[Key] }>{};
 
   for (const key of Object.keys(ReferenceObject)) {
     if (!(key in CompareObject)) {
-      result[key] = ReferenceObject[key];
+      result[key as Exclude<keyof T, keyof K>] = ReferenceObject[key];
     }
   }
 
-  return result as Pick<T, UniqueKeys>;
+  return result;
 }
 
-console.log(difference(a, b));
+let res = Difference(a, b);
+console.log(res);
